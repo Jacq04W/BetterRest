37da5829889f89dd00b197404c8aeeda53e70bf2
Jacquese <jacqwhitson@gmail.com>
2022-12-24 19:16:01 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Jacquese Whitson  on 12/24/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
